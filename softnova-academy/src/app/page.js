963559d7766ba08b74b3babe1e65b6d1c6861c64
Akileswaran-Dev <akileswaran.dev@gmail.com>
2026-04-21 16:38:19 +0530

    startproject-210426

---
+++ softnova-academy/src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div style={{textAlign:'center'}}>
+      
+    <h2>Welcome Our <b>Softnova Academy</b> Website</h2>
+    <p>All the best</p>
+    </div>
+  );
+}
